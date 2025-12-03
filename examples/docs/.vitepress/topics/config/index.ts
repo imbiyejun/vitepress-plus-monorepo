a320b777219c2f9e56a87cb5072a3ad021923b7b
biyejun <bnbiye@163.com>
2025-12-03 23:30:51 +0800

    chore: Remove obsolete documentation files and examples from VitePress Plus, streamlining project structure

--- examples/docs/.vitepress/topics/config/index.ts
+++
@@ -1,4 +0,0 @@
-// Import topics config from template
-export { topics } from '../../../../../packages/vitepress-plus/template/docs/.vitepress/topics/config'
-export * from '../../../../../packages/vitepress-plus/template/docs/.vitepress/topics/config/types'
-
--- examples/docs/.vitepress/topics/data/index.ts
+++
@@ -1,4 +0,0 @@
-// Import topics data from template
-export { topicsData } from '../../../../../packages/vitepress-plus/template/docs/.vitepress/topics/data'
-export * from '../../../../../packages/vitepress-plus/template/docs/.vitepress/topics/data/types'
-
